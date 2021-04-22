e5e5bec4f0bb0588d8d18f7c8eedc1bb0579f55f
Ian Mungai <iansam74@gmail.com>
2021-04-23 01:29:11 +0300

    Upload Image to web3 test

--- src/contracts/Decentragram.sol
+++ src/contracts/Decentragram.sol
@@ -1,5 +1,25 @@
-pragma solidity ^0.5.0;
+// SPDX-License-Identifier: <SPDX-License>
+pragma solidity >=0.4.22 <0.9.0;
 
 contract Decentragram {
-  // Code goes here...
+    string public name = "Decentragram";
+
+    // Store Images
+    mapping(uint256 => Image) public images;
+
+    struct Image {
+        uint256 id;
+        string hash;
+        string description;
+        uint256 tipAmount;
+        address payable author;
+    }
+
+    // Create Images
+
+    function uploadImage() public {
+        images[1] = Image(1, "123", "Hello World", 0, address(uint160(0x0)));
+    }
+
+    // Tip Images
 }
